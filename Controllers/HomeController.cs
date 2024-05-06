cfa67dc0577c7df7d523c7399bbcb8449e86178c
BlackOut 921 <conorhowe921@gmail.com>
2024-05-06 11:14:51 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_net8_food.Controllers
+{
+	[AllowAnonymous]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index() => View();
+	}
+}
