4514528130908815947da0908f98d37f606adb05
BlackOut 921 <conorhowe921@gmail.com>
2024-05-07 22:23:37 +0100

    offline icons

--- wwwroot/JavaScript.js
+++ wwwroot/JavaScript.js
@@ -10,6 +10,7 @@
 });
 
 //Get all elements that use bo-scroll-target (the elements that will animate in)
+//Better as an animation?? can be delayed...
 const _fadeElements = document.querySelectorAll(".bo-scroll-target");
 if (_fadeElements.length > 0) {
     _fadeElements.forEach(i => {
